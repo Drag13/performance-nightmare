63337fe85a8fc144a6a0281384240861d4ef2938
drag13 <no-questions@github.noreply.com>
2023-01-11 14:27:48 +0200

    Added moment and content to the second page

--- src/second-page/second.page.jsx
+++ src/second-page/second.page.jsx
@@ -1,6 +1,12 @@
+import moment from "moment";
+
 export const SecondPage = () => (
   <article>
     <h1 className="align-center">Second page</h1>
-    <p>Content goes here</p>
+    <p className="align-center">
+      Сьогодні{" "}
+      <strong>{moment([2022, 1, 24]).diff(new Date(), "days") * -1}</strong>{" "}
+      день війни
+    </p>
   </article>
 );
