ddf3dd4e144576333f618b065d4c88ddfee48f8c
drag13 <no-questions@github.noreply.com>
2023-01-12 10:56:35 +0200

    Updated readme Increased prime to 600

--- src/main-page/main.loader.js
+++ src/main-page/main.loader.js
@@ -2,7 +2,7 @@ import { API_BASE_PATH } from "../config";
 
 export async function mainPageLoader() {
   const defaultPrimeValue = await fetch(
-    `${API_BASE_PATH}/factorial/initialValue/4000`
+    `${API_BASE_PATH}/factorial/initialValue/6000`
   ).then((r) => r.json());
 
   return { defaultPrimeValue: defaultPrimeValue.value };
