4608ee1a1afda69d7260796ec313a20064f6a13a
drag13 <no-questions@github.noreply.com>
2023-01-12 11:54:18 +0200

    Added all locales from moment

--- src/second-page/second.page.jsx
+++ src/second-page/second.page.jsx
@@ -1,12 +1,31 @@
-import moment from "moment";
+import moment from "moment/min/moment-with-locales";
+import { Switch, FormControlLabel } from "@mui/material";
+import { useState } from "react";
 
-export const SecondPage = () => (
-  <article>
-    <h1 className="align-center">Second page</h1>
-    <p className="align-center">
-      Сьогодні{" "}
-      <strong>{moment([2022, 1, 24]).diff(new Date(), "days") * -1}</strong>{" "}
-      день війни
-    </p>
-  </article>
-);
+export const SecondPage = () => {
+  const [lang, setLang] = useState("en");
+  const now = moment(new Date()).locale(lang);
+
+  const switchLanguage = (e) => {
+    setLang(e.target.checked ? "uk" : "en");
+  };
+
+  return (
+    <article>
+      <h1 className="align-center">War in Ukraine</h1>
+      <FormControlLabel
+        control={<Switch onChange={switchLanguage} />}
+        label="Ukrainian"
+      />
+      <p className="align-center">
+        Today is{" "}
+        <strong>
+          {now.format("MMMM")}, {now.format("YYYY")} рік
+          {", "}
+          {moment([2022, 1, 24]).diff(now, "days") * -1}
+        </strong>{" "}
+        день війни
+      </p>
+    </article>
+  );
+};
